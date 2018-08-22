86de5122cca871b6c6d5938f790469d681d8d435
gaolei <gaolei@xianglin.cn>
2018-08-22 13:22:15 +0800

    建立 项目 目录，配置webpack打包工具

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+/**
+ * Created by apple on 2018/8/22.
+ *
+ * 游戏打包配置
+ */
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const WEBPACK_PRO_ENV = process.env.NODE_ENV;
+
+const sourcePath = {
+    'dev':path.join(__dirname,'./server/views'),
+    'dist':path.join(__dirname,'./dist')
+}
+
+module.exports = {
+    entry:path.join(__dirname,'./src/game.js'),
+    devtool:'cheap-module-source-map',
+    output:{
+        path:sourcePath[WEBPACK_PRO_ENV],
+        publicPath:'/',
+        filename:'js/[name]_[chunkhash:8].js',
+        chunkFilename:'js/common/[name]_[chunkhash:8].js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["env", "stage-2"],
+                        plugins: ['transform-runtime']
+                    }
+                }
+            },
+            {
+                test:/\.(png|svg|jpg|gif|eot|woff|ttf)$/,
+                use:['file-loader?name=[path][name]_[hash:8].[ext]&context=./src/images&outputPath=images/']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html',
+            filename:'index.html',
+            showErrors:true,
+            inject:'body'
+        })
+    ]
+}
